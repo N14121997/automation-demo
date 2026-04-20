47b1e288e168eaf8703d48c924e2f141d2243110
Nooran Eslam <nooran14121997@gmail.com>
2026-04-20 23:53:08 +0200

    fix headless mode

--- src/test/java/GoogleTest.java
+++ src/test/java/GoogleTest.java
@@ -1,5 +1,6 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -7,7 +8,15 @@ public class GoogleTest {
 
     @Test
     public void testGoogleTitle() {
-        WebDriver driver = new ChromeDriver();
+
+        // إعدادات Chrome عشان يشتغل في CI (GitHub Actions)
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless"); // تشغيل بدون UI
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
+
         driver.get("https://www.google.com");
 
         String title = driver.getTitle();
