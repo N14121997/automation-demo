992f43cea26a859d5df034091a90bd5d6071d82f
Nooran Eslam <nooran14121997@gmail.com>
2026-04-20 23:19:35 +0200

    first commit

---
+++ src/test/java/GoogleTest.java
@@ -0,0 +1,18 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+
+    @Test
+    public void testGoogleTitle() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+
+        String title = driver.getTitle();
+        Assert.assertTrue(title.contains("Google"));
+
+        driver.quit();
+    }
+}
